97abf97353e5eeb1bf74c5ffe99d04412750b3b2
devchoi <cnhhw0408@gmail.com>
2025-05-29 10:47:14 +0900

    :sparkles: feat: aws s3, transcribe setting + implements

--- src/main/java/com/team_3/nursing_care/common/response/ResponseDto.java
+++ src/main/java/com/team_3/nursing_care/common/response/ResponseDto.java
@@ -1,7 +1,6 @@
 package com.team_3.nursing_care.common.response;
 
 import com.team_3.nursing_care.common.exception.ResultMessage;
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
