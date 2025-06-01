b792ea881a0675b908f57db00b2d93574840ee04
minah13 <hdmksm1@naver.com>
2025-06-02 00:04:25 +0900

    :hammer: refactor: 서비스명 수정

--- src/main/java/com/team_3/nursing_care/domain/member/controller/CaregiverController.java
+++ src/main/java/com/team_3/nursing_care/domain/member/controller/CaregiverController.java
@@ -14,12 +14,12 @@ import org.springframework.web.multipart.MultipartFile;
 @RequiredArgsConstructor
 public class CaregiverController {
 
-    private final CaregiverService memberService;
+    private final CaregiverService caregiverService;
 
     @PostMapping("/caregiver")
     public ResponseEntity<?> createCaregiver(@Validated @ModelAttribute CaregiverInfoRequestDto dto, @RequestPart(value="profileImage", required = false) MultipartFile profileImage) {
 
-        memberService.addCaregiver(dto, Role.CAREGIVER, profileImage);
+        caregiverService.addCaregiver(dto, Role.CAREGIVER, profileImage);
         return ResponseEntity.ok("새로운 요양보호사가 정상적으로 등록 되었습니다.");
     }
 
