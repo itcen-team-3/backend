e79d4134c5c86bee2372c47c1fc0137316b14ad1
minah13 <hdmksm1@naver.com>
2025-06-01 23:44:40 +0900

    :sparkles: feat: 기본이미지 추가 및 이미지경로 수정

--- src/main/java/com/team_3/nursing_care/domain/member/controller/CaregiverController.java
+++ src/main/java/com/team_3/nursing_care/domain/member/controller/CaregiverController.java
@@ -3,14 +3,11 @@ package com.team_3.nursing_care.domain.member.controller;
 import com.team_3.nursing_care.domain.member.constant.Role;
 import com.team_3.nursing_care.domain.member.dto.request.CaregiverInfoRequestDto;
 import com.team_3.nursing_care.domain.member.service.CaregiverService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/v1/member")
@@ -20,8 +17,9 @@ public class CaregiverController {
     private final CaregiverService memberService;
 
     @PostMapping("/caregiver")
-    public ResponseEntity<?> createCaregiver(@Validated @ModelAttribute CaregiverInfoRequestDto dto) {
-        memberService.addCaregiver(dto, Role.CAREGIVER);
+    public ResponseEntity<?> createCaregiver(@Validated @ModelAttribute CaregiverInfoRequestDto dto, @RequestPart(value="profileImage", required = false) MultipartFile profileImage) {
+
+        memberService.addCaregiver(dto, Role.CAREGIVER, profileImage);
         return ResponseEntity.ok("새로운 요양보호사가 정상적으로 등록 되었습니다.");
     }
 
