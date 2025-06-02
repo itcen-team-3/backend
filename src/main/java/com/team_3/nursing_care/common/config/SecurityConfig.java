06f7c374d60853196de5d479165c7ef481247be5
devchoi <cnhhw0408@gmail.com>
2025-06-02 19:45:38 +0900

    :hammer: TASK: deploy env setting

--- src/main/java/com/team_3/nursing_care/common/config/SecurityConfig.java
+++ src/main/java/com/team_3/nursing_care/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,11 +17,12 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/v1/**").permitAll() // TODO: Spring Security 설정 후 수정
                         .anyRequest().authenticated()
                 )
+                .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
--- src/main/java/com/team_3/nursing_care/common/proxy/S3ServiceImpl.java
+++ src/main/java/com/team_3/nursing_care/common/proxy/S3ServiceImpl.java
@@ -1,6 +1,5 @@
 package com.team_3.nursing_care.common.proxy;
 
-import com.team_3.nursing_care.common.exception.CareLogException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.ResponseInputStream;
 import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.http.HttpStatusCode;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -79,7 +77,7 @@ public class S3ServiceImpl implements S3Service {
     }
 
     private void checkEmptyFile(MultipartFile file) {
-        if (file.isEmpty()) throw new CareLogException(HttpStatusCode.BAD_REQUEST, "File is empty");
+        if (file.isEmpty()) throw new RuntimeException("File is empty");
     }
 
     private String createS3Key(MultipartFile file) {
@@ -103,7 +101,7 @@ public class S3ServiceImpl implements S3Service {
         try (InputStream is = file.getInputStream()) {
             s3Client.putObject(request, RequestBody.fromInputStream(is, file.getSize()));
         } catch (IOException e) {
-            throw new CareLogException(HttpStatusCode.INTERNAL_SERVER_ERROR, "Error while uploading file");
+            throw new RuntimeException("Error while uploading file");
         }
     }
 }
