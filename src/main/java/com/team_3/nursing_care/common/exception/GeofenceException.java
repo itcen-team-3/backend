13b1d8cf8c83c17a956665ea22837dbf728c6451
devchoi <cnhhw0408@gmail.com>
2025-06-03 17:22:22 +0900

    :sparkles: Geofence crud, Util, dto

---
+++ src/main/java/com/team_3/nursing_care/common/exception/GeofenceException.java
@@ -0,0 +1,9 @@
+package com.team_3.nursing_care.common.exception;
+
+
+public class GeofenceException extends CustomException {
+
+    public GeofenceException(int code, String message) {
+        super(code, message);
+    }
+}
