eb4e08096d5ac2f3bafc0005d19934ea123ed4a3
devchoi <cnhhw0408@gmail.com>
2025-06-05 11:26:36 +0900

    :sparkles: GlobalExceptionHandler 관리할 exception 추가

--- src/main/java/com/team_3/nursing_care/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/team_3/nursing_care/common/exception/GlobalExceptionHandler.java
@@ -1,10 +1,12 @@
 package com.team_3.nursing_care.common.exception;
 
 import com.team_3.nursing_care.common.response.ResponseDto;
+import jakarta.validation.UnexpectedTypeException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -32,5 +34,50 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(ex.getCode())
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(ex.toResponseDto());
+    }
+
+    @ExceptionHandler(CareLogException.class)
+    public ResponseEntity<ResponseDto<?>> handleCareLogException(CareLogException ex){
+        log.warn("❗ CareLogException error: {}",ex.getMessage());
+
+        return ResponseEntity.status(ex.getCode())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ex.toResponseDto());
+    }
+
+    @ExceptionHandler(MemberException.class)
+    public ResponseEntity<ResponseDto<?>> handleMemberException(MemberException ex){
+        log.warn("❗ MemberException error: {}",ex.getMessage());
+
+        return ResponseEntity.status(ex.getCode())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ex.toResponseDto());
+    }
+
+    @ExceptionHandler(GeofenceException.class)
+    public ResponseEntity<ResponseDto<?>> handleGeofenceException(GeofenceException ex){
+        log.warn("❗ GeofenceException error: {}",ex.getMessage());
+
+        return ResponseEntity.status(ex.getCode())
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(ex.toResponseDto());
+    }
+
+    @ExceptionHandler(UnexpectedTypeException.class)
+    public ResponseEntity<ResponseDto<?>> handleUnexpectedTypeException(UnexpectedTypeException ex){
+        log.warn("❗ UnexpectedTypeException error: {}",ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ResponseDto<>(HttpStatusCode.BAD_REQUEST, ResultMessage.Error, ex.getMessage()));
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<ResponseDto<?>> handleUsernameNotFoundException(UsernameNotFoundException ex){
+        log.warn("❗ UsernameNotFoundException error: {}",ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ResponseDto<>(HttpStatusCode.BAD_REQUEST, ResultMessage.Error, ex.getMessage()));
     }
 }
