cb78e2970bc60eedbb51ecf0cabdad3e25e3571a
chaewon <woneveryday@naver.com>
2025-06-09 16:16:25 +0900

    :hammer: refactor: 요양 보호사 name list 조회 시 프로필 이미지 같이 넘겨주게 수정

--- src/main/java/com/team_3/nursing_care/domain/member/dto/response/CaregiversNameResponseDto.java
+++ src/main/java/com/team_3/nursing_care/domain/member/dto/response/CaregiversNameResponseDto.java
@@ -3,21 +3,26 @@ package com.team_3.nursing_care.domain.member.dto.response;
 import com.team_3.nursing_care.domain.member.entity.Member;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CaregiversNameResponseDto {
 
     private Long caregiverId;
     private String caregiverName;
+    private String caregiverProfileUrl;
 
     @Builder
-    public CaregiversNameResponseDto(Long caregiverId, String caregiverName) {
+    public CaregiversNameResponseDto(Long caregiverId, String caregiverName, String caregiverProfileUrl) {
+        this.caregiverProfileUrl=caregiverProfileUrl;
         this.caregiverId = caregiverId;
         this.caregiverName = caregiverName;
     }
 
     public static CaregiversNameResponseDto from(Member member) {
         return CaregiversNameResponseDto.builder()
+                .caregiverProfileUrl(member.getProfileImageUrl())
                 .caregiverId(member.getMemberId())
                 .caregiverName(member.getMemberName())
                 .build();
