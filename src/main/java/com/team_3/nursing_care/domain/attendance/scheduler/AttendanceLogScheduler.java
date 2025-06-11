ecb2d3a06b5bfee0bb1e4fbbf549f6b980d69996
devchoi <cnhhw0408@gmail.com>
2025-06-11 09:14:11 +0900

    :sparkles: attendanceLog 관련 스케줄러 로직 추가

---
+++ src/main/java/com/team_3/nursing_care/domain/attendance/scheduler/AttendanceLogScheduler.java
@@ -0,0 +1,48 @@
+package com.team_3.nursing_care.domain.attendance.scheduler;
+
+import com.team_3.nursing_care.common.exception.CustomException;
+import com.team_3.nursing_care.domain.attendance.constant.CheckInStatus;
+import com.team_3.nursing_care.domain.attendance.constant.CheckOutStatus;
+import com.team_3.nursing_care.domain.attendance.entity.AttendanceLog;
+import com.team_3.nursing_care.domain.attendance.repository.AttendanceLogRepository;
+import com.team_3.nursing_care.domain.schedule.entity.Schedule;
+import com.team_3.nursing_care.domain.schedule.repository.ScheduleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.http.HttpStatusCode;
+
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AttendanceLogScheduler {
+
+    private final AttendanceLogRepository attendanceLogRepository;
+    private final ScheduleRepository scheduleRepository;
+
+    @Scheduled(cron = "0 30 23 * * ?")
+    public void attendanceLogSchedule() {
+        List<AttendanceLog> saveList = new ArrayList<>();
+
+        attendanceLogRepository.findAllForSchedule().forEach(al -> {
+            Schedule schedule = scheduleRepository.findByAttendanceLog(al.getMember(), al.getPatientId(), al.getCheckIn().toLocalDate()).orElseThrow(() -> new CustomException(HttpStatusCode.NOT_FOUND, "로그 기록 상 일치하는 것이 되는 스케줄이 없다."));
+
+            if (schedule.getStartTime().after(Time.valueOf(al.getCheckIn().toLocalTime())))
+                al.updateCheckInStatus(CheckInStatus.ON_TIME);
+            else al.updateCheckInStatus(CheckInStatus.LATE);
+
+            if (Time.valueOf(al.getCheckOut().toLocalTime()).before(schedule.getEndTime()))
+                al.updateCheckOutStatus(CheckOutStatus.ON_TIME);
+            else al.updateCheckOutStatus(CheckOutStatus.EARLY_LEAVE);
+
+            saveList.add(al);
+        });
+
+        attendanceLogRepository.saveAll(saveList);
+    }
+}
